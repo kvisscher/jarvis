3ab03f9f1b9caba149376b9da02a52f43d627a49
Kevin Visscher <tensilecolt@gmail.com>
2013-05-11 23:01:56 +0200

    Not sure, what I've changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,34 @@
 #include <pulse/pulseaudio.h>
 #include <pulse/simple.h>
-
 #include <pocketsphinx.h>
-
 #include "speak_lib.h"
 
 #include <stdio.h>
-
 #include <string.h>
-
 #include <time.h>
 
 #define BUF_SIZE 1024 << 6
+
+#define APPLICATION_NAME "Jarvis"
 
 #define JARVIS_ACOUSTIC_MODEL "hmm/en_US/hub4wsj_sc_8k"
 #define JARVIS_DICT "9133.dic"
 #define JARVIS_LANG_MODEL "7859.lm"
 
+// Time in seconds that the user has to give jarvis a command
+// after waking him up
+#define JARVIS_TIME_TO_OBEY 5
+
 pa_simple* playback = NULL;
+
+// Time when the command mode was last activated
+time_t command_mode_activated = 0;
 
 int espeak_synth_callback(short* wav, int numsamples, espeak_EVENT* events)
 {
 	printf("event type: %d, num_samples: %d\n", events->type, numsamples);
 
-	// Is there a sample to play?
+	// Are there samples to play?
 	if (numsamples == 0)
 		return 0;	
 	
@@ -43,15 +48,16 @@ int espeak_synth_callback(short* wav, int numsamples, espeak_EVENT* events)
 
 void say(char const* word)
 {
-	espeak_Synth(word,
-	 	    strlen(word) + 1,
-	 	    0,
-	 	    POS_CHARACTER,
-	 	    0,
-	 	    espeakCHARS_AUTO, 
-	 	    NULL, 
-	 	    (void*) playback
- 	    );
+	espeak_Synth(
+		word,
+		strlen(word) + 1,
+	 	0,
+	 	POS_CHARACTER,
+	 	0,
+	 	espeakCHARS_AUTO, 
+	 	NULL, 
+	 	(void*) playback
+	);
 	
 	if (espeak_Synchronize() < 0)
 	{
@@ -61,28 +67,46 @@ void say(char const* word)
 
 void handle_word(char const* word)
 {
-	if (strcmp(word, "JARVIS") == 0)
+	printf("handle_word() %s", word);
+
+	if (command_mode_activated > 0)
 	{
-		say("Yes sir? What would you have me do?");
-	}
-	else if (strcmp(word, "TIME") == 0)
-	{
-		time_t t = time(NULL);
-		tm* now = localtime(&t);
+		time_t now = time(NULL);
+		time_t diff = now - command_mode_activated;
 		
-		char buf[128];
+		if (diff / 1000 < JARVIS_TIME_TO_OBEY)
+		{
+			if (strcmp(word, "TIME") == 0)
+			{
+				time_t t = time(NULL);
+				tm* now = localtime(&t);
 		
-		strftime(buf, sizeof(buf), "Todays date is %A %e %B %G", now);
+				char buf[128];
 		
-		say(buf);
-	}
-	else if (strcmp(word, "HELLO") == 0)
-	{
-		say("Hello");
+				strftime(buf, sizeof(buf), "Todays date is %A %e %B %G", now);
+		
+				say(buf);
+			}
+		}
+		
+		command_mode_activated = 0;
 	}
 	else
 	{
-		printf("Didn't recgonize command: %s\n", word);
+		if (strcmp(word, "JARVIS") == 0)
+		{
+			command_mode_activated = time(NULL);
+	
+			say("Yes sir? What would you have me do?");
+		}
+		else if (strcmp(word, "HELLO") == 0)
+		{
+			say("Hello");
+		}
+		else
+		{
+			printf("Didn't recgonize command: %s\n", word);
+		}
 	}
 }
 
@@ -101,24 +125,23 @@ int main()
 	playback_spec.channels = 2;
 	playback_spec.rate = 8000;
 	
-	playback = pa_simple_new(NULL,               // Use the default server.
-		          "Fooap22p",           // Our application's name.
-		          PA_STREAM_PLAYBACK,
-		          NULL,               // Use the default device.
-		          "Playback",            // Description of our stream.
-		          &playback_spec,                // Our sample format.
-		          NULL,               // Use default channel map
-		          NULL,               // Use default buffering attributes.
-		          NULL               // Ignore error code.
-		         );
+	playback = pa_simple_new(
+			  NULL, 		// Use the default server.
+		          APPLICATION_NAME,     // Our application's name.
+		          PA_STREAM_PLAYBACK,   // Use this server to play sound
+		          NULL,                 // Use the default device.
+		          "Jarvis mouth",       // Description of our stream.
+		          &playback_spec,       // Our sample format.
+		          NULL,                 // Use default channel map
+		          NULL,                 // Use default buffering attributes.
+		          NULL                  // Ignore error code.
+			);
 
+	// Let espeak use the english voice
 	espeak_SetVoiceByName("english-us");
-
+	
+	// Callback handler for synth
 	espeak_SetSynthCallback(&espeak_synth_callback);
-
-	espeak_VOICE* voice = espeak_GetCurrentVoice();
-
-	printf("current voice: %s\n", voice->name);
 
 	printf("espeak sample rate: %d\n", sample_rate);
 
@@ -129,38 +152,45 @@ int main()
 	ss.channels = 1;
 	ss.rate = 16000;
 
-	s = pa_simple_new(NULL,               // Use the default server.
-		          "Fooapp",           // Our application's name.
-		          PA_STREAM_RECORD,
-		          NULL,               // Use the default device.
-		          "Music",            // Description of our stream.
-		          &ss,                // Our sample format.
-		          NULL,               // Use default channel map
-		          NULL,               // Use default buffering attributes.
-		          NULL               // Ignore error code.
-		         );
+	s = pa_simple_new(
+			NULL,               // Use the default server.
+		        APPLICATION_NAME,   // Our application's name.
+		        PA_STREAM_RECORD,   // Use the server to record audio samples
+		        NULL,               // Use the default device.
+		        "Jarvis ears",      // Description of our stream.
+		        &ss,                // Our sample format.
+		        NULL,               // Use default channel map
+		        NULL,               // Use default buffering attributes.
+		        NULL               // Ignore error code.
+		);
 
 	ps_decoder_t* ps;
 	cmd_ln_t* config;
-
-	config = cmd_ln_init(NULL, ps_args(), TRUE,
-	                     "-hmm", JARVIS_ACOUSTIC_MODEL,
-	                     "-lm", JARVIS_LANG_MODEL,
-	                     "-dict", JARVIS_DICT,
-	                     NULL);
+	
+	// Load pocketsphinx config
+	config = cmd_ln_init(
+			NULL,
+			ps_args(),
+			TRUE,
+	                "-hmm", JARVIS_ACOUSTIC_MODEL,
+	                "-lm", JARVIS_LANG_MODEL,
+	                "-dict", JARVIS_DICT,
+	                NULL
+		);
 
 	if (config == NULL)
+	{
+		printf("error while initializing pocketsphinx config");
 		return 1;
-
+	}
+	
 	ps = ps_init(config);
 
 	if (ps == NULL)
 	{
-		printf("error initializing ps");
+		printf("error initializing pocketsphinx decoder");
 		return 1;	
 	}
-
-	char const* uttid = "utt_test";
 
 	for (;;)
 	{
@@ -169,55 +199,65 @@ int main()
 
 		if (pa_simple_read(s, buf, BUF_SIZE, &error) < 0)
 		{
-			printf("error while reading: %s\n", pa_strerror(error));
+			printf("error while reading input: %s\n", pa_strerror(error));
 			return 1;
 		}
 			
-		printf("read sample\n");
-	
-
 		if (ps_start_utt(ps, "test") < 0)
 		{
 			printf("error starting utterance");
 			return 1;
 		}	
 	
-		int rv = ps_process_raw(ps, (short*) buf, BUF_SIZE / 2, 0, 0);
-		
-		if (rv < 0)
+		// Dividing the buffer size by 2 because of the cast to short
+		if (ps_process_raw(ps, (short*) buf, BUF_SIZE / 2, 0, 1) < 0)
 		{
-			printf("error decoding raw data: %d", rv);
+			printf("error decoding raw data");
 			return 1;
 		}
 
-		if (ps_end_utt(ps))
+		if (ps_end_utt(ps) < 0)
 		{
 			printf("error ending utterance");
 			return 1;
 		}
 
+		// The word pocketsphinx decoded from the audio sample
 		char const* hyp;
+		
+		// Not sure what the score is meant to indicate
 		int score;
 	
-		hyp = ps_get_hyp(ps, &score, &uttid);
+		hyp = ps_get_hyp(ps, &score, NULL);
 
 		if (hyp == NULL || strlen(hyp) == 0)
 		{
+			// pocketsphinx was unable to make up
+			// anything from the sample
+			
 			continue;
 		}
 
 		printf("SCORE: %d", score);
 
 		// TODO: Listen for more words?
-
 		handle_word(hyp);	
 	}
 
+	if (ps)
+	{
+		ps_free(ps);
+	}
+
 	if (s)
+	{
 		pa_simple_free(s);
+	}
 	
 	if (playback)
+	{
 		pa_simple_free(playback);
-
+	}
+	
 	return 0;        
 }
